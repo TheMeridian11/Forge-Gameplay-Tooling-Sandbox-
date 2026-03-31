d0345b548a2e866268617b37aaed11f1478ccab8
TheMeridian11 <ProjectFrostBite2043@gmail.com>
2026-03-31 23:27:20 +1100

    added some extra comments adding clarification to code design choices and explaining OOP principles utilised

--- include/simulation/SimulationState.h
+++ include/simulation/SimulationState.h
@@ -4,6 +4,20 @@
 
 #include "gameplay/Unit.h"
 
+// following good OOP/Encapsulation, (public vs private)
+// I wnated to hide internal data and only expose the operations we want others to use.
+// this makes the class easier to manage as it grows.
+
+// I also would like to clarify the different ways 'const' is used here.
+// for example:
+// (1) const Unit& unit:
+// This parameter is read only.
+// (2) const std::vector<Unit>&:
+// The returned reference is read only
+// (3) getUnits() const:
+// The member fucntion itself is read only with respect to the object 
+
+
 // Holds the current state of the sandbox simulation
 // For phase 2, it mainly stores units and allows new ones to be added. 
 class SimulationState {
@@ -17,6 +31,8 @@ class SimulationState {
     // Returns the total number of units in the simulation.
     std::size_t getUnitCount() const;
 
+    // m_units is private because it is the internal state of the class
+    // we do not want outside code doing anything it wants directly to this vector.
     private:
     std::vector<Unit> m_units;
 };
